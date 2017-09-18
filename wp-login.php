4ff9d1ed9c472491989a577612d42e77fc16729f
masterclass2017 <31978727+masterclass2017@users.noreply.github.com>
2017-09-18 18:54:00 -0400

    nueva pagina

---
+++ wp-login.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="es-ES">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="es-ES">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Politics &#038; Power Master Class &lsaquo; Acceder</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.1' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="/wp-content/uploads/2017/09/cropped-icono-1-32x32.png" sizes="32x32" />
+<link rel="icon" href="/wp-content/uploads/2017/09/cropped-icono-1-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="/wp-content/uploads/2017/09/cropped-icono-1-180x180.png" />
+<meta name="msapplication-TileImage" content="http://127.0.0.1/wp/wp-content/uploads/2017/09/cropped-icono-1-270x270.png" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-es-es">
+		<div id="login">
+		<h1><a href="https://es.wordpress.org/" title="Funciona gracias a WordPress" tabindex="-1">Politics &#038; Power Master Class</a></h1>
+	
+<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Nombre de usuario o dirección de correo electrónico<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Contraseña<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Recuérdame</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Acceder" />
+		<input type="hidden" name="redirect_to" value="http://127.0.0.1/wp/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="/wp-login.php?action=lostpassword">¿Has perdido tu contraseña?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+/**
+ * Filters whether to print the call to `wp_attempt_focus()` on the login screen.
+ *
+ * @since 4.8.0
+ *
+ * @param bool $print Whether to print the function call. Default true.
+ */
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="/">&larr; Volver a Politics &#038; Power Master Class</a></p>
+	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
